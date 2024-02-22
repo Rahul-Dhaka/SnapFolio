2a661c1de2b497cf7d62e8be688758fde5e74301
Rahul Dhaka <dhakarahul16@gmail.com>
2024-02-22 16:28:51 +0530

    auth with CSS

--- routes/router.js
+++ routes/router.js
@@ -1,28 +1,21 @@
 const express = require('express');
 const router = new express();
-const loginController = require('../controller/login')
-const signupController = require('../controller/signup');
+const authController = require('../controller/auth');
+const sectionsController = require('../controller/sections');
 const myPassport = require('../auth/myPassport')
 
 router.get('/', (req,res)=>{
     res.render('index');
 })
-router.get('/login', loginController.getLogin);
-router.post('/login', myPassport.authenticate('local', { failureRedirect: '/login' }),
-function (req, res) {
+
+router.post('/login', myPassport.authenticate('local', { failureRedirect: '/login' }),function (req, res) {
     res.redirect('/profile');
 });
-router.get('/logout', function(req, res, next) {
-    req.logout(function(err) {
-      if (err) { return next(err); }
-      res.redirect('/login');
-    });
-  });
 
-router.get('/signup', signupController.getSignup);
-router.post('/signup', signupController.postSignup);
-router.get('/profile', (req,res)=>{
-    res.render('profile' , {username: req.user.username});
-  })
+router.get('/login', authController.getLogin);
+router.get('/logout', authController.getLogout);
+router.get('/signup', authController.getSignup);
+router.post('/signup', authController.postSignup);
+router.get('/profile', sectionsController.getProfile)
 
 module.exports = router;
